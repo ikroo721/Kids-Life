7cc2db65fa8651f84f0f2ca9bb6c5f6887fb5d54
Mohammad Ikram <ikramullah201812345@gmail.com>
2024-08-04 20:40:13 +0500

    Your commit message

--- src/Components/FooterProductHandler.jsx
+++ src/Components/FooterProductHandler.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const FooterProductHandler = ({children}) => {
   return (
-    <div className='min-h-[20vh]  sm:h-[50vh] md:h-[60vh] lg:h-[45vh] sm:w-[70vw]  
+    <div className='min-h-[20vh]  w-[80%] sm:h-[50vh] md:h-[60vh] lg:h-[45vh] sm:w-[80vw]  
     md:w-[22vw] lg:w-[17.3vw]  flex flex-col gap-6  ' >
           {children}
     </div>
